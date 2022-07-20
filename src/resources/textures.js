0ef45e40b3e661750476a5e97540b8f0d44ad59c
Vedant Raval <76436755+vedantraval2310@users.noreply.github.com>
2022-07-20 21:09:24 +0530

    Add files via upload

---
+++ src/resources/textures.js
@@ -0,0 +1,52 @@
+//billboardTextures
+let billboardTextures = {};
+
+//box textures
+let boxTexture = {};
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.twitter = '../src/jsm/twitter.png';
+boxTexture.LinkedIn = '../src/jsm/linkedInLogo.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.globe = '../src/jsm/thunder.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.allSkills = '../src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/skrillex.png';
+boxTexture.edmText = '../src/jsm/EDM.png';
+boxTexture.writing = '../src/jsm/writing.png';
+
+//material textures
+let stoneTexture = '../src/jsm/stone.png';
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+//text
+let inputText = {};
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+//URLs
+let URL = {};
+URL.terpsolutions =
+  '';
+URL.ryanfloyd = '';
+URL.bagholderBets = '';
+URL.homeSweetHomeURL = '';
+URL.gitHub = '';
+URL.twitter = '';
+URL.LinkedIn = '';
+URL.email = 'vedant.k.raval@gmail.com';
+URL.githubBagholder = 'https://github.com/MrRyanFloyd/bagholder-bets';
+URL.githubHomeSweetHome = 'https://github.com/MrRyanFloyd/home-sweet-127.0.0.1';
+URL.devTo =
+  '';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
